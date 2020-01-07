bda74a0f97fc9a1aaf35bb4a0b4b49e55facd4d4
Richard <richardnixon@posteo.net>
2020-01-06 23:56:35 -0300

    exec 11

--- Python/EstruturaSequencial.py
+++ Python/EstruturaSequencial.py
@@ -1,53 +1,63 @@
-#01
-print("Ola Mundo!")
+# #01
+# print("Ola Mundo!")
 
-#02
-num = input("Digite um número:")
-print(f"o numero digitado foi:{num}")
+# #02
+# num = input("Digite um número:")
+# print(f"o numero digitado foi:{num}")
 
-#03
-n1 = int(input("Digite um primeiro número: "))
-n2 = int(input("Digite um segundo número: "))
-print(n1 + n2)
+# #03
+# n1 = int(input("Digite um primeiro número: "))
+# n2 = int(input("Digite um segundo número: "))
+# print(n1 + n2)
 
-#04
-msg = ("Digite a ")
-nota1 = int(input(msg + "primeira nota: "))
-nota2 = int(input(msg + "segunda nota: "))
-nota3 = int(input(msg + "terceira nota: "))
-nota4 = int(input(msg + "quarta nota: "))
-print(nota1 + nota2 + nota3 + nota4 /4)
+# #04
+# msg = ("Digite a ")
+# nota1 = int(input(msg + "primeira nota: "))
+# nota2 = int(input(msg + "segunda nota: "))
+# nota3 = int(input(msg + "terceira nota: "))
+# nota4 = int(input(msg + "quarta nota: "))
+# print(nota1 + nota2 + nota3 + nota4 /4)
 
-#05
-mt = int(input("Digite a quantidade em metros: "))
-print(f"É equivalente a: {mt * 100} Cm")
+# #05
+# mt = int(input("Digite a quantidade em metros: "))
+# print(f"É equivalente a: {mt * 100} Cm")
 
-#06
-raio = float(input("Entre com o valor do raio: "))
-circunferencia = 2 * 3.14 * raio
-area = 3.14 * (raio * raio)
-print("Área: {:.2f}, Raio: {:.2f}, Circunferencia: {:.2f}".format(area, raio, circunferencia))
+# #06
+# raio = float(input("Entre com o valor do raio: "))
+# circunferencia = 2 * 3.14 * raio
+# area = 3.14 * (raio * raio)
+# print("Área: {:.2f}, Raio: {:.2f}, Circunferencia: {:.2f}".format(area, raio, circunferencia))
 
-#07
-b = int(input("Digite o Valor da base: "))
-h = int(input("Digite o Valor da altura: "))
+# #07
+# b = int(input("Digite o Valor da base: "))
+# h = int(input("Digite o Valor da altura: "))
 
-if b != h :
-    print("O quadro deve ter a base = altura!")
-else :
-    print(f"Área do quadrado é:{b * h * 2}")
+# if b != h :
+#     print("O quadro deve ter a base = altura!")
+# else :
+#     print(f"Área do quadrado é:{b * h * 2}")
 
-#08
-salario = float(input("Quanto você ganha por hora ? "))
-horas = float(input("Quantas horas voce trabalha por mes ? "))
-print(f"Voce recebeu o equivalente a: {horas * salario}")
+# #08
+# salario = float(input("Quanto você ganha por hora ? "))
+# horas = float(input("Quantas horas voce trabalha por mes ? "))
+# print(f"Voce recebeu o equivalente a: {horas * salario}")
 
-#09
-f = float(input("Qual valor Farenheit? "))
-c = (f-32) / 1.8
-print(f"Convertido para Celsius é equivalente a: {c}")
+# #09
+# f = float(input("Qual valor Farenheit? "))
+# c = (f-32) / 1.8
+# print(f"Convertido para Celsius é equivalente a: {c}")
 
-#10
-c = float(input("Qual valor Celsius? "))
-f = (c * 1.8) + 32
-print(f"Convertido para Farenheit é equivalente a: {f}")
+# #10
+# c = float(input("Qual valor Celsius? "))
+# f = (c * 1.8) + 32
+# print(f"Convertido para Farenheit é equivalente a: {f}")
+
+#11
+int_1 = int(input("Digite um valor inteio: "))
+int_2 = int(input("Digite outro valor INTEIRO: "))
+real_1 = float(input("digite um valor real! "))
+print((2 * int_1) + int_2 / 2)
+print((3 * int_1) + real_1)
+print(real_1 ** 3)
+
+#12
